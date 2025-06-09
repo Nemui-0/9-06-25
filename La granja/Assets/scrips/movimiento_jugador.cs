7e02a96ccc2b97316ff87d3b5476e80d4fd086fb
Nemui <diana.coyotl@ucic.edu.mx>
2025-06-09 12:58:18 -0600

    V0

---
+++ La granja/Assets/scrips/movimiento_jugador.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movimiento_jugador : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public Rigidbody2D rg;
+    public Vector2 entrada;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rg.linearVelocity = entrada * velocidad;
+    }
+public void Moverse(InputAction.CallbackContext contexto){
+    entrada = contexto.ReadValue<Vector2>();
+}
+}

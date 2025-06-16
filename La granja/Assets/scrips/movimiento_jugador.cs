7851498b801a6b340354e6776215719f4cc026ba
Nemui <diana.coyotl@ucic.edu.mx>
2025-06-16 12:56:48 -0600

    V1

--- La granja/Assets/scrips/movimiento_jugador.cs
+++ La granja/Assets/scrips/movimiento_jugador.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class movimiento_jugador : MonoBehaviour
-{
+
     public float velocidad = 5f;
-    public Rigidbody2D rg;
+    public Rigidbody2D rb;
     public Vector2 entrada;
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rg = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>();
     }
+        
 
     // Update is called once per frame
     void Update()
@@ -19,6 +22,32 @@ public class movimiento_jugador : MonoBehaviour
         rg.linearVelocity = entrada * velocidad;
     }
 public void Moverse(InputAction.CallbackContext contexto){
-    entrada = contexto.ReadValue<Vector2>();
-}
+
+    animator.SetBool("estaCaminando", true); 
+
+    Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+    // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+
+        animator.SetFloat("estradaX", entrada.x);
+        animator.SetFloat("entraday", entrada.y);
+
+        if(contexto.canceled){
+            animator.SetBool("estaCaminando", false); 
+    }
+
 }
